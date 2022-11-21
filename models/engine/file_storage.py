d91d95f8b2f1381249a5e516e363fd515d73b25f
= <=>
2022-11-21 19:24:14 +0100

    File Storage II

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,7 @@ an object"""
 
 import json
 from models.base_model import BaseModel
+from .errors import *
 
 
 class FileStorage:
@@ -51,4 +52,21 @@ class FileStorage:
                     FileStorage.models[obj["__class__"]](**obj)
                     for obj in deserialized}
         except FileNotFoundError:
+            # No need for error
             pass
+
+    def find_by_id(self, model, obj_id):
+        """Find and return an elemt of model by its id"""
+        F = FileStorage
+        if model not in F.models:
+            # Invalid Model Name
+            # Not yet Implemented
+            raise ModelNotFoundError(model)
+
+        key = model + "." + obj_id
+        if key not in F.__objects:
+            # invalid id
+            # Not yet Implemented
+            raise InstanceNotFoundError(obj_id, model)
+
+        return F.__objects[key]
